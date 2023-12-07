62ba6b2def568de459dcf1390907d0b11fc0baf1
Stefan Klessinger <stefan.klessinger@posteo.de>
2023-12-07 20:11:55 +0100

    Make harness match other ones more closely

--- Harness.scala
+++ Harness.scala
@@ -1,4 +1,5 @@
 // TODO: Prints
+// TODO: "throws" in Scala?
 import java.io._
 import io.circe.generic.auto._
 import io.circe.generic.semiauto._
@@ -13,7 +14,7 @@ import java.util.Scanner
 class Harness {
   val NOT_IMPLEMENTED: String = "This case is not yet implemented."
 
-  val SKIPPED_CASES: Map[String, String] = Map(
+  val UNSUPPORTED_CASES: Map[String, String] = Map(
     "escaped pointer ref" -> NOT_IMPLEMENTED,
     "empty tokens in $ref json-pointer" -> NOT_IMPLEMENTED,
     "schema that uses custom metaschema with with no validation vocabulary" -> NOT_IMPLEMENTED,
@@ -22,86 +23,75 @@ class Harness {
     "$ref to $dynamicRef finds detached $dynamicAnchor" -> NOT_IMPLEMENTED
   )
 
-  val SKIPPED_TESTS: Map[String, TestSkip] = Map(
+  // List of specific tests of a case that are not supported
+  val UNSUPPORTED_TESTS: Map[String, TestSkip] = Map(
     "minLength validation" -> TestSkip("one supplementary Unicode code point is not long enough", NOT_IMPLEMENTED),
     "maxLength validation" -> TestSkip("two supplementary Unicode code points is long enough", NOT_IMPLEMENTED)
   )
 
-  implicit val customConfig: Configuration = Configuration.default.withDefaults
-
   def operate(line: String) = {
-    try {
-      val node: Json = parse(line) match {
-        case Right(json)          => json
-        case Left(parsingFailure) => throw parsingFailure
-      }
+    val node: Json = parse(line) match {
+      case Right(json)          => json
+      case Left(parsingFailure) => throw parsingFailure
+    }
 
-      val cmd: String = (node \\ "cmd").headOption match {
-        case Some(value) => value.asString.getOrElse("")
-        case None        => throw new RuntimeException("Failed to get cmd")
-      }
-      cmd match {
-        case "start"   => println(start(node))
-        case "dialect" => println(dialect(node))
-        case "run"     => println(run(node))
-        case "stop"    => System.exit(0)
-        case default   => throw new IllegalArgumentException(s"Unknown command: $default")
-      }
-    } catch {
-      case e: Exception =>
-        // TODO: Check if this is the correct way to handle errors
-        println(Errored(e.getMessage(), e.getStackTrace().mkString("\n")).asJson.noSpaces)
+    val cmd: String = (node \\ "cmd").headOption match {
+      case Some(value) => value.asString.getOrElse("")
+      case None        => throw new RuntimeException("Failed to get cmd")
+    }
+    cmd match {
+      case "start"   => println(start(node))
+      case "dialect" => println(dialect(node))
+      case "run"     => println(run(node))
+      case "stop"    => System.exit(0)
+      case default   => throw new IllegalArgumentException(s"Unknown command: $default")
     }
   }
 
   def start(node: Json): String = {
+    Harness.started = true
     val startRequest: StartRequest = decodeTo[StartRequest](node)
-    val version = startRequest.version
-    if (version == 1) {
-      Harness.started = true
-      val is: InputStream = getClass.getResourceAsStream("META-INF/MANIFEST.MF")
-      val attributes = new Manifest(is).getMainAttributes
-
-      val dialects: Json = Json.arr(Json.fromString("https://json-schema.org/draft/2020-12/schema"))
-
-      val implementation: Json = Json.obj(
-        "language" -> Json.fromString("scala"),
-        "name" -> Json.fromString(attributes.getValue("Implementation-Name")),
-        "version" -> Json.fromString(attributes.getValue("Implementation-Version")),
-        "homepage" -> Json.fromString("https://gitlab.lip6.fr/jsonschema/modernjsonschemavalidator"),
-        "issues" -> Json.fromString("https://gitlab.lip6.fr/jsonschema/modernjsonschemavalidator/issues"),
-        "dialects" -> dialects,
-        "os" -> Json.fromString(System.getProperty("os.name")),
-        "os_version" -> Json.fromString(System.getProperty("os.version")),
-        "language_version" -> Json.fromString(Runtime.version().toString)
-      )
-
-      StartResponse(version, true, implementation).asJson.noSpaces
-    } else {
-      // TODO: should return an errored response?
-      throw new IllegalArgumentException(s"Unsupported version: ${startRequest.version}")
+    if (startRequest.version != 1) {
+      throw new IllegalArgumentException(s"Unsupported IHOP version: ${startRequest.version}")
     }
+
+    val is: InputStream = getClass.getResourceAsStream("META-INF/MANIFEST.MF")
+    val attributes = new Manifest(is).getMainAttributes
+
+    val implementation: Implementation = Implementation(
+      "scala",
+      attributes.getValue("Implementation-Name"),
+      attributes.getValue("Implementation-Version"),
+      List("https://json-schema.org/draft/2020-12/schema"),
+      "https://gitlab.lip6.fr/jsonschema/modernjsonschemavalidator",
+      "https://gitlab.lip6.fr/jsonschema/modernjsonschemavalidator/issues",
+      System.getProperty("os.name"),
+      System.getProperty("os.version"),
+      Runtime.version().toString,
+      List()
+    )
+    StartResponse(startRequest.version, true, implementation).asJson.noSpaces
   }
 
   def dialect(node: Json): String = {
     if (!Harness.started) {
-      throw new RuntimeException("Bowtie hasn't started!")
+      throw new IllegalArgumentException("Not started!")
     }
-    val dialectRequest: DialectRequest = decodeTo[DialectRequest](node)
-    // TODO: Handle properly (if this is not correct); We should probably return true
-    DialectResponse(false).asJson.noSpaces
+    
+    DialectResponse(true).asJson.noSpaces
   }
 
   def run(node: Json): String = {
     if (!Harness.started) {
-      throw new RuntimeException("Bowtie hasn't started!")
+      throw new IllegalArgumentException("Not started!")
     }
 
     val runRequest: RunRequest = decodeTo[RunRequest](node)
+    // TODO: check try/catch and error handling
     try {
       val caseDescription = runRequest.testCase.description
-      if (SKIPPED_CASES.contains(caseDescription)) {
-        return SkippedRunResponse(runRequest.seq, true, Some(NOT_IMPLEMENTED)).asJson.noSpaces
+      if (UNSUPPORTED_CASES.contains(caseDescription)) {
+        return RunSkippedResponse(runRequest.seq, true, Some(NOT_IMPLEMENTED)).asJson.noSpaces
       }
 
       val registryMap: Map[String, String] = runRequest.testCase.registry
@@ -113,33 +103,23 @@ class Harness {
 
       var resultArray = Vector.empty[Json]
       runRequest.testCase.tests.foreach { test =>
-        try {
-          val testDescription: String = test.description
-          val instance: String = test.instance.noSpaces
+        val testDescription: String = test.description
+        val instance: String = test.instance.noSpaces
 
-          if (SKIPPED_TESTS.contains(caseDescription) && SKIPPED_TESTS(caseDescription).description == testDescription) {
-            resultArray :+= SkippedTest(message = Some(SKIPPED_TESTS(caseDescription).message)).asJson
-          } else {
-            val schema: String = runRequest.testCase.schema.noSpaces
+        if (UNSUPPORTED_TESTS.contains(caseDescription) && UNSUPPORTED_TESTS(caseDescription).description == testDescription) {
+          resultArray :+= SkippedTest(message = Some(UNSUPPORTED_TESTS(caseDescription).message)).asJson
+        } else {
+          val schema: String = runRequest.testCase.schema.noSpaces
 
-            val result: Json = Json.obj("valid" -> MainClass.validateInstance(schema, instance, registryMap).asJson)
-            resultArray :+= result
-          }
-        } catch {
-          // TODO: Testing
-          case e: Exception => {
-            val error: Errored = Errored(e.getMessage(), e.getStackTrace().mkString("\n"))
-            resultArray :+= ErroredTest(true, error).asJson
-          }
-        }
+          val result: Json = Json.obj("valid" -> MainClass.validateInstance(schema, instance, registryMap).asJson)
+          resultArray :+= result
+        }      
       }
       RunResponse(runRequest.seq, resultArray).asJson.noSpaces
-
     } catch {
-      // TODO: We currently abort instead of creating errored responses?
       case e: Exception =>
-        val error: Errored = Errored(e.getMessage(), e.getStackTrace().mkString("\n"))
-        ErroredRunResponse(runRequest.seq, context = error).asJson.noSpaces
+        val error: ErrorContext = ErrorContext(e.getMessage(), e.getStackTrace().mkString("\n"))
+        RunErroredResponse(runRequest.seq, context = error).asJson.noSpaces
     }
   }
 
@@ -151,15 +131,16 @@ class Harness {
   }
 }
 
-// TODO: Check if we need any more case classes
+case class Implementation(language: String, name: String, version: String, 
+                          dialects: List[String], homepage: String, issues: String, 
+                          os: String, os_version: String, language_version: String,
+                          links: List[Link])
 
-case class Test(description: String, comment: Option[String], instance: Json, valid: Option[Boolean])
-
-case class TestCase(description: String, comment: Option[String], schema: Json, registry: Option[Json], tests: List[Test])
+case class Link(url: String, description: String)
 
 case class StartRequest(version: Int)
 
-case class StartResponse(version: Int, ready: Boolean, implementation: Json)
+case class StartResponse(version: Int, ready: Boolean, implementation: Implementation)
 
 case class DialectRequest(dialect: String)
 
@@ -174,13 +155,15 @@ object RunRequest {
 
 case class RunResponse(seq: Json, results: Vector[Json])
 
-case class ErroredRunResponse(seq: Json, errored: Boolean = true, context: Errored)
+case class RunSkippedResponse(seq: Json, skipped: Boolean = true, message: Option[String] = None)
 
-case class ErroredTest(errored: Boolean = true, context: Errored) // TODO: restructure?
+case class RunErroredResponse(seq: Json, errored: Boolean = true, context: ErrorContext)
 
-case class Errored(message: String, traceback: String) // TODO: needed?
+case class ErrorContext(message: String, traceback: String)
 
-case class SkippedRunResponse(seq: Json, skipped: Boolean = true, message: Option[String] = None)
+case class TestCase(description: String, comment: Option[String], schema: Json, registry: Option[Json], tests: List[Test])
+
+case class Test(description: String, comment: Option[String], instance: Json, valid: Option[Boolean])
 
 case class SkippedTest(skipped: Boolean = true, message: Option[String] = None)
 
